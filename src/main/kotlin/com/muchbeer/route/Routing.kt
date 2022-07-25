dc845c0a93faa8f5e585e52e6280d742b328e618
muchbeer <gman2them@gmail.com>
2022-07-25 14:35:27 +0300

    make big changes

--- src/main/kotlin/com/muchbeer/route/Routing.kt
+++ src/main/kotlin/com/muchbeer/route/Routing.kt
@@ -10,7 +10,6 @@ import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import org.koin.ktor.ext.inject
 import org.ktorm.database.Database
 
 fun Application.configureRouting() {
--- src/main/kotlin/com/muchbeer/route/Serialization.kt
+++ src/main/kotlin/com/muchbeer/route/Serialization.kt
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.databind.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
 
 fun Application.configureSerialization() {
 
